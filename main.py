fed2624eb16814609ecfb3cc1f5647b84cb91bc6
Inesso4ka103 <innaalpar@gmail.com>
2025-01-18 15:59:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ async def put_user(
     user_id: Annotated[int, Path(ge=0,le=100, discription='Enter User ID')],
     username: Annotated[str, Path(min_length=5, max_length=20,discription='Enter username')],
     age: Annotated[int, Path(ge=18, le=120, discription='Enter age')]) -> str:
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated'
 
 @app.delete('/users/{user_id}')
 async def delete_user(user_id: Annotated[int, Path(ge=0,le=100, discription='Enter User ID')]) -> str:
-    users.pop(str(user_id))
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+    users.pop(user_id)
     return f'User {user_id} has been deleted'
